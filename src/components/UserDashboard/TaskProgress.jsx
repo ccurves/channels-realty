22152f92af309bce376bfbd19e6f226b63bd330b
ccurves <simoncovenant47@gmail.com>
2022-10-16 15:14:43 +0100

    Worked on google auth

--- src/components/UserDashboard/TaskProgress.jsx
+++ src/components/UserDashboard/TaskProgress.jsx
@@ -6,7 +6,7 @@ const TaskProgress = () => {
     <div>
       <TaskProgressComponent sqm={150} perc="4" />
       <TaskProgressComponent sqm={300} perc="2" />
-      <TaskProgressComponent sqm={500} perc="1" />
+      <TaskProgressComponent sqm={600} perc="1" />
     </div>
   );
 };
