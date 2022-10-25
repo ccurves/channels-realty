5a264e41816744c11dce8a911b82fa72b86f7c23
ccurves <simoncovenant47@gmail.com>
2022-10-25 13:37:28 +0100

    Worked on google auth

--- src/components/Auth/GoogleSuccess.jsx
+++ src/components/Auth/GoogleSuccess.jsx
@@ -9,46 +9,46 @@ const GoogleSuccess = () => {
 
   useEffect(() => {
     const getUser = () => {
-      // axios
-      //   .get(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
-      //     // mode: "cors",
-      //     withCredentials: true,
-      //     headers: {
-      //       Accept: "application/json",
-      //       "Content-Type": "application/json",
-      //     },
-      //   })
-      //   .then((res) => {
-      //     console.log(res);
-      //     // authenticate(res.data, () => {
-      //     //   isAuth && navigate("/");
-      //     // });
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //     // setError(err.response.data.errors);
-      //   });
-
-      fetch(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
+          // mode: "cors",
+          withCredentials: true,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         })
-        .then((resObject) => {
-          authenticate(resObject, () => {
-            isAuth && navigate("/");
-          });
+        .then((res) => {
+          console.log(res);
+          // authenticate(res.data, () => {
+          //   isAuth && navigate("/");
+          // });
         })
         .catch((err) => {
           console.log(err);
+          // setError(err.response.data.errors);
         });
+
+      // fetch(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
+      //   method: "GET",
+      //   credentials: "include",
+      //   headers: {
+      //     Accept: "application/json",
+      //     "Content-Type": "application/json",
+      //   },
+      // })
+      //   .then((response) => {
+      //     if (response.status === 200) return response.json();
+      //     throw new Error("authentication has been failed!");
+      //   })
+      //   .then((resObject) => {
+      //     authenticate(resObject, () => {
+      //       isAuth && navigate("/");
+      //     });
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
     };
     getUser();
   }, [navigate]);
