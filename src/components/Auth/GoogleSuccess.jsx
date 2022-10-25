3de88a386f1c75499ea81c538a754c7b7f54007b
ccurves <simoncovenant47@gmail.com>
2022-10-25 03:25:28 +0100

    Worked on google auth

--- src/components/Auth/GoogleSuccess.jsx
+++ src/components/Auth/GoogleSuccess.jsx
@@ -1,4 +1,5 @@
 import { CheckCircle } from "@mui/icons-material";
+import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { authenticate, isAuth } from "../../helpers/auth";
@@ -8,27 +9,45 @@ const GoogleSuccess = () => {
 
   useEffect(() => {
     const getUser = () => {
-      fetch(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
+          withCredentials: true,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         })
-        .then((resObject) => {
-          console.log(resObject);
-          authenticate(resObject, () => {
+        .then((res) => {
+          console.log(res);
+          authenticate(res.data, () => {
             isAuth && navigate("/");
           });
         })
         .catch((err) => {
           console.log(err);
+          // setError(err.response.data.errors);
         });
+
+      // fetch(`${process.env.REACT_APP_API_URL}/auth/login/success`, {
+      //   method: "GET",
+      //   credentials: "include",
+      //   headers: {
+      //     Accept: "application/json",
+      //     "Content-Type": "application/json",
+      //   },
+      // })
+      //   .then((response) => {
+      //     if (response.status === 200) return response.json();
+      //     throw new Error("authentication has been failed!");
+      //   })
+      //   .then((resObject) => {
+      //     authenticate(resObject, () => {
+      //       isAuth && navigate("/");
+      //     });
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
     };
     getUser();
   }, [navigate]);
